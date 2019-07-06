97ef306f5564ed5d125c894a430e2a8fc842a972
soopark <sparkm0128@gmail.com>
2019-07-06 21:18:55 +0900

    RFT: change loading directory of story files

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,7 +2,7 @@ import { configure } from '@storybook/react';
 
 // automatically import all files ending in *.stories.js
 const req = require.context(
-  '../src/components',
+  '../stories',
   true,
   /\.stories\.(ts|tsx)$/,
 );
--- stories/KeyboardListView.stories.tsx
+++ stories/KeyboardListView.stories.tsx
@@ -1,9 +1,13 @@
 import * as React from 'react';
 
 import { storiesOf } from '@storybook/react';
-import KeyboardListView from './KeyboardListView';
+import KeyboardListView from '../src/components/KeyboardListView';
   
 storiesOf('KeyboardListView', module)
 .add('test', () => (
+  <KeyboardListView />
+)).add('test1', () => (
+  <KeyboardListView />
+)).add('test2', () => (
   <KeyboardListView />
 ));
