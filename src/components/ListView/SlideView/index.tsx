6cacffc1edf34c9f6b76d79adca48fddad00a719
soopark <sparkm0128@gmail.com>
2019-07-08 09:34:42 +0900

    ENH: add contens-row, contents-wrapper in SlideView

--- src/components/ListView/SlideView/index.tsx
+++ src/components/ListView/SlideView/index.tsx
@@ -24,8 +24,20 @@ class SlideView extends React.Component<ISlideViewProps, ISlideViewState> {
   }
 
   render() {
+    const {slideList, activeContent} = this.state;
     return (
-      
+      <div className="contents-row" id={`active-content-${activeContent.index}`}>
+        <div className="contents-wrapper" style={{
+          // 화면 크기에 유동적일 수 있도록 수정 필요할 듯(6이 아니라 수학적인 수식으로)
+          'transform': `translateX(-${activeContent.index<slideList.length-6 && 
+          activeContent.index*(100/slideList.length)}%)`
+        }}>
+          {
+            // list 렌덩링 
+            // slideList.map(content => <Content key={content._id} content={content} />)
+          }
+        </div>
+      </div>
     );
   }
 };
