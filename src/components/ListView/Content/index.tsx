b58b940f5d2a67057a11297968c5c4e4cdba3762
soopark <sparkm0128@gmail.com>
2019-07-08 09:48:49 +0900

    ENH: add Content component

---
+++ src/components/ListView/Content/index.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { IContent } from 'types';
+import './style.scss';
+
+interface IContentProps {
+  content: IContent
+}
+
+const Content = (props: IContentProps) => {
+  return (
+  );
+};
+
+export default Content;
