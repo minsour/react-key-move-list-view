109ae0d8f1a182d0c847efeb687724b0769cb869
soopark <sparkm0128@gmail.com>
2019-07-09 11:05:40 +0900

    ENH: add fake data

---
+++ data/index.ts
@@ -0,0 +1,74 @@
+const data = [
+  {
+      "_id": "593e9297e17df20c4a237d42",
+      "index": 0,
+      "image": "https://ihatetomatoes.net/demos/_rw/01-real-estate/tn_property01.jpg",
+      "title": "Singer",
+      "description": "914 Argyle Road 914 Argyle Road 914 Argyle Road ",
+  },
+  {
+      "_id": "593e9297ec4cca9c56bf61af",
+      "index": 1,
+      "image": "https://ihatetomatoes.net/demos/_rw/01-real-estate/tn_property02.jpg",
+      "title": "Machias",
+      "description": "255 Raleigh Place",
+  },
+  {
+      "_id": "593e929773c71925e5d7c11c",
+      "index": 2,
+      "image": "https://ihatetomatoes.net/demos/_rw/01-real-estate/tn_property03.jpg",
+      "title": "Bend",
+      "description": "580 Amber Street",
+  },
+  {
+      "_id": "593e92973d4d15eedb129aee",
+      "index": 3,
+      "image": "https://ihatetomatoes.net/demos/_rw/01-real-estate/tn_property04.jpg",
+      "title": "Ivanhoe",
+      "description": "854 Varanda Place",
+  },
+  {
+      "_id": "593e929728b7f8543b7907e1",
+      "index": 4,
+      "image": "https://ihatetomatoes.net/demos/_rw/01-real-estate/tn_property05.jpg",
+      "title": "Lydia",
+      "description": "857 Rockaway Parkway",
+  },
+  {
+      "_id": "593e92975996a6263d146444",
+      "index": 5,
+      "image": "https://ihatetomatoes.net/demos/_rw/01-real-estate/tn_property01.jpg",
+      "title": "Rosedale",
+      "description": "543 Harman Street",
+  },
+  {
+      "_id": "593e9297a13e246da5ba0884",
+      "index": 6,
+      "image": "https://ihatetomatoes.net/demos/_rw/01-real-estate/tn_property02.jpg",
+      "title": "Allison",
+      "description": "787 Gerald Court",
+  },
+  {
+      "_id": "593e9297afa51a013fcd07d5",
+      "index": 7,
+      "image": "https://ihatetomatoes.net/demos/_rw/01-real-estate/tn_property03.jpg",
+      "title": "Nicut",
+      "description": "416 Vandervoort Avenue",
+  },
+  {
+      "_id": "593e92972bd8fd05f2ab1a89",
+      "index": 8,
+      "image": "https://ihatetomatoes.net/demos/_rw/01-real-estate/tn_property04.jpg",
+      "title": "Canterwood",
+      "description": "437 Mill Street",
+  },
+  {
+      "_id": "593e9297fe2e6d32c859ecf4",
+      "index": 9,
+      "image": "https://ihatetomatoes.net/demos/_rw/01-real-estate/tn_property05.jpg",
+      "title": "Cobbtown",
+      "description": "553 Friel Place",
+  }
+]
+
+export default data;
