ce63357c2de20e2f817c92e7f61d465c1442f3ff
devsoopark <sparkm0128@gmail.com>
2019-07-21 19:05:57 +0900

    RFT: apply custom hooks

--- src/components/ListView/SlideView/index.tsx
+++ src/components/ListView/SlideView/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import Content from '../Content';
 import { IContent } from 'types';
 import './style.scss'
-import { KEY_DOWN, LEFT_KEY, RIGHT_KEY } from '../../../constants';
+import { useCurrentContent } from '../../../hooks';
 
 interface ISlideViewProps { 
   contentWidth?: number | string;
@@ -10,81 +10,28 @@ interface ISlideViewProps {
   list: IContent[];
 }
 
-interface ISlideViewState {
-  slideList: IContent[];
-  activeContent: IContent;
-}
-
-class SlideView extends React.Component<ISlideViewProps, ISlideViewState> {
-  constructor(props: ISlideViewProps) {
-    super(props);
-    this.state = {
-      slideList: this.props.list,
-      activeContent: this.props.list[0]
-    }
-  }
-
-  componentDidMount = () => {
-    document.addEventListener(KEY_DOWN, this.onKeyDown);
-  };
-
-  componentWillUnmount = () => {
-    document.removeEventListener(KEY_DOWN, this.onKeyDown);
-  };
-
-  right = () => {
-    if(this.props.list[this.state.activeContent.index+1] !== undefined)
-    {
-      const newIndex = this.state.activeContent.index+1;
-      this.setState({
-        activeContent: this.props.list[newIndex]
-      })
-    }
-  }
-
-  left = () => {
-    if(this.props.list[this.state.activeContent.index-1] !== undefined)
-    {
-      const newIndex = this.state.activeContent.index-1;
-      this.setState({
-        activeContent: this.props.list[newIndex]
-      })
-    }
-  }
-  // event Type 지정 (any → KeyboardEvent)
-  private onKeyDown = (event: KeyboardEvent) => {
-		switch (event.keyCode) {
-			case LEFT_KEY:
-				this.left();
-				break;
-			case RIGHT_KEY:
-				this.right();
-				break;
-		}
-  };
-
-  render() {
-    const {slideList, activeContent} = this.state;
+const SlideView = (props: ISlideViewProps) => {
+  const current = useCurrentContent(props.list.length);
+  console.log(current.x);
     return (
-      <div className="contents-row" id={`active-content-${activeContent.index}`}>
+      <div className="slide-row" id={`active-content-${current.x}`}>
         <div className="focus-box" style={{
           // 화면 크기에 유동적일 수 있도록 수정 필요할 듯(7이 아니라 수학적인 수식으로)
-          'transform': `translateX(${activeContent.index>=slideList.length-7 && 
-          (activeContent.index-(slideList.length-7))*100}%)`
+          'transform': `translateX(${current.x>=props.list.length-7 && 
+          (current.x-(props.list.length-7))*100}%)`
         }}/>
-        <div className="contents-wrapper" style={{
+        <div className="slide-wrapper" style={{
           // 화면 크기에 유동적일 수 있도록 수정 필요할 듯(6이 아니라 수학적인 수식으로)
-          'transform': `translateX(-${activeContent.index<slideList.length-6 && 
-          activeContent.index*(100/slideList.length)}%)`
+          'transform': `translateX(-${current.x<props.list.length-6 && 
+          current.x*(100/props.list.length)}%)`
         }}>
           {
             // list 렌더링 
-            slideList.map(content => <Content key={content._id} content={content} />)
+            props.list.map(content => <Content key={content._id} content={content} />)
           }
         </div>
       </div>
     );
-  }
-};
+}
 
 export default SlideView;
