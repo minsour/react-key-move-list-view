70c31f6d7dc9f871e02ce1ca1e63620723ccea5f
devsoopark <sparkm0128@gmail.com>
2019-07-24 20:00:54 +0900

    RFT: change KEY_EVENT to EVENT

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,7 +1,7 @@
-import { KEY_EVENT, LEFT_KEY, RIGHT_KEY, UP_KEY, DOWN_KEY} from './keyboard';
+import { EVENT, LEFT_KEY, RIGHT_KEY, UP_KEY, DOWN_KEY} from './keyboard';
 
 export {
-  KEY_EVENT,
+  EVENT,
   LEFT_KEY,
   RIGHT_KEY,
   UP_KEY,
