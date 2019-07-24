94d4df6c0b6bdcfe9d9249708d52736e2c6d8a4e
devsoopark <sparkm0128@gmail.com>
2019-07-24 20:15:09 +0900

    RFT: separate FocusBox from SlideView and MatrixView

---
+++ src/components/ListView/FocusBox/index.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import { ICurrent, IFocusBox, VIEW_TYPE } from '../../../types';
+
+interface IFocusBoxProps { 
+  pageCol: number;
+  pageRow?: number;
+  contentCol: number;
+  current: ICurrent;
+  focus: IFocusBox;
+  type: VIEW_TYPE;
+}
+
+const FocusBox = (props: IFocusBoxProps) => {
+  const { pageCol, pageRow, contentCol, current, focus } = props;
+  const moveFocusBox = {
+    'transform': props.type === VIEW_TYPE.SLIDE ?
+      `translateX(${current.x >= contentCol - pageCol && (current.x-(contentCol-pageCol))*100}%)` :
+      `translate(${current.x  < pageCol ? (current.x*100):((pageCol-1)*100)}%, ${current.y< pageRow! ? (current.y*100):((pageRow!-1)*100)}%)`
+  };
+
+  return (
+    <div className="focus-box" style={moveFocusBox} ref={focus.ref}/>
+  );
+}
+
+export default FocusBox;
--- src/components/ListView/SlideView/index.tsx
+++ src/components/ListView/SlideView/index.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
-import {useCallback, useState} from 'react';
 import Content from '../Content';
-import { IContent } from 'types';
+import { IContent, VIEW_TYPE } from '../../../types';
 import './style.scss'
-import { useCurrentContent, useWindowDimensions } from '../../../hooks';
+import { useCurrentContent, useWindowDimensions, useFocusbox } from '../../../hooks';
+import FocusBox from '../FocusBox';
 
 interface ISlideViewProps { 
   contentWidth?: number | string;
@@ -14,31 +14,30 @@ interface ISlideViewProps {
 const SlideView = (props: ISlideViewProps) => {
   const current = useCurrentContent(props.list.length);
   const windowDimensions = useWindowDimensions();
-  const [focusBoxDimensions, setFocusBoxDimensions] = useState({width:0, height:0});
-    const focusBoxRef = useCallback(ref => {
-        if(ref !== null){
-            setFocusBoxDimensions({width:ref.offsetWidth, height:ref.offsetHeight});
-            window.addEventListener('resize',()=>setFocusBoxDimensions({width:ref.offsetWidth, height:ref.offsetHeight}));
-        }
-    },[]);
-    const pageCol = Math.floor(windowDimensions.width/focusBoxDimensions.width);
-    const contentCol = props.list.length;
+  const focus = useFocusbox();
+  const pageCol = Math.floor(windowDimensions.width/focus.offsetWidth);
+  const contentCol = props.list.length;
+  
+  const moveSlideWrapper = {
+    'transform': `translateX(-${current.x< contentCol-pageCol+1 && current.x*(100/contentCol)}%)`
+  };
+
+  const renderContents = () => (
+    props.list.map(content => <Content key={content._id} content={content} />)
+  );
+
   console.log(current.x);
     return (
       <div className="slide-row" id={`active-content-${current.x}`}>
-        <div className="focus-box" style={{
-          // 화면 크기에 유동적일 수 있도록 수정 필요할 듯(7이 아니라 수학적인 수식으로)
-          'transform': `translateX(${current.x >= contentCol - pageCol && (current.x-(contentCol-pageCol))*100}%)`
-        }} ref = {focusBoxRef}/>
-        <div className="slide-wrapper" style={{
-          // 화면 크기에 유동적일 수 있도록 수정 필요할 듯(6이 아니라 수학적인 수식으로)
-          'transform': `translateX(-${current.x< contentCol-pageCol+1 && 
-          current.x*(100/contentCol)}%)`
-        }}>
-          {
-            // list 렌더링 
-            props.list.map(content => <Content key={content._id} content={content} />)
-          }
+        <FocusBox
+          pageCol={pageCol}
+          contentCol={contentCol}
+          current={current}
+          focus={focus}
+          type={VIEW_TYPE.SLIDE}
+        />
+        <div className="slide-wrapper" style={moveSlideWrapper}>
+          {renderContents()}
         </div>
       </div>
     );
