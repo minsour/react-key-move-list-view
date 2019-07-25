7d167234c9c3f311e1b8c52b87672382f6e4febe
devsoopark <sparkm0128@gmail.com>
2019-07-25 20:54:13 +0900

    ENH: add IViewState interface

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,9 +1,11 @@
 import { VIEW_TYPE } from './ListView';
 import { IContent } from './Content';
-import { ICurrent, IFocusBox } from './Focus';
+import { ICurrent, IFocusBox, IViewState } from './Focus';
+
 export {
   VIEW_TYPE,
   IContent,
   ICurrent,
   IFocusBox,
+  IViewState,
 };
