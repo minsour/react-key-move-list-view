abc5ab8ec47332cbd8cb2e09d4f97c7874ba24cd
devsoopark <sparkm0128@gmail.com>
2019-07-28 19:48:39 +0900

    RFT: change inside of Content based on styled components

--- src/components/ListView/Content/index.tsx
+++ src/components/ListView/Content/index.tsx
@@ -1,27 +1,68 @@
 import * as React from 'react';
 import { IContent } from 'types';
 import './style.scss';
+import { Div } from '../../atoms/Div';
+import { Image } from '../../atoms/Image';
+import { P } from '../..//atoms/P';
+import { Span } from '../..//atoms/Span';
 
 interface IContentProps {
   content: IContent
+  translateX?: number
 }
 
 const Content = (props: IContentProps) => {
   const {index, image, title, description} = props.content;
   return (
-    <div id = {`content-${index}`} className = "content">
-      <div className = "image-wrapper">
-        <img src={image} alt={title} />
-        <p className="details">
-          <span className="title">
+    <div id = {`content-${index}`} className = "content" style={{left: index*189, transform: `translateX(-${props.translateX}%)`}}>
+      {/* <div className = "image-wrapper"> */}
+      <Div
+        position="relative"
+      >
+        <Image
+          src={image}
+          alt={title}
+          border="0"
+          width="13vw"
+          height="10vw"
+        />
+        <P
+          padding="0"
+          margin="0"
+          lineHeight={1}
+          position="absolute"
+          bottom="5px"
+          background="rgba(0,0,0,0.5)"
+          width="13vw"
+          height="50px"
+        >
+          <Span
+            padding="0"
+            margin="0 0 5px 0"
+            lineHeight={1}
+            position="absolute"
+            zIndex={1}
+            top="5px"
+            left="5px"
+            fontSize="18px"
+            fontWeight="bold"
+            color="#fff"
+          >
             {title}
-          </span>
-          <span className="description">
+          </Span>
+          <Span
+            position="absolute"
+            fontSize="11px"
+            color="#fff"
+            left="5px"
+            top="25px"
+          >
             {description}
-          </span>
-        </p>
+          </Span>
+        </P>
+        </Div>
       </div>
-    </div>
+    // </div>
   );
 };
 
