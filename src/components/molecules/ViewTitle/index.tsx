b9434ba559b7d29bbb55c0987aa6be78b41b6d07
devsoopark <sparkm0128@gmail.com>
2019-07-31 20:04:41 +0900

    CLN: change GREY color

--- src/components/molecules/ViewTitle/index.tsx
+++ src/components/molecules/ViewTitle/index.tsx
@@ -1,20 +1,25 @@
 import * as React from 'react';
+import { THEME } from '../../../constants';
+import { THEME_STRING } from '../../../constants';
 
 interface IViewTitleProps {
   action?: boolean;
   focus?: boolean;
   title?: string;
+  theme?: string;
 }
 
 const ViewTitle = (props: IViewTitleProps) => {
+  const theme = (props.theme === void 0) ? THEME[THEME_STRING.LIGHT] : THEME[props.theme];
+
   return(
     <div 
       style={{
+        height: '20px',
+        margin: '15px 20px',
         fontWeight: 'bold', 
         fontSize: '18px', 
-        color: props.action ? 
-          '#4DA6F6' : 
-          (props.focus ? '#F29661' : '#606060')
+        color: props.action ? theme.ACTION : (props.focus ? theme.FOCUS : theme.DEFAULT)
       }}
     >
       {props.focus && props.focus!==props.action ? `> ${props.title}` : props.title}
