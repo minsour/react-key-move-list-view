38a44125d9a80df3cd365f84773b454cd0ddb4a7
devsoopark <sparkm0128@gmail.com>
2019-07-31 10:36:07 +0900

    RFT: add SlideView story docs

--- stories/ListView/SlideView.stories.tsx
+++ stories/ListView/SlideView.stories.tsx
@@ -2,14 +2,16 @@ import * as React from 'react';
 import { storiesOf } from '@storybook/react';
 import ListView from '../../src/components/organisms/ListView';
 import {useFetch} from '../apis/fetchUrl';
+import ShowDocs from '../utils/ShowDocs';
 
 const DefaultSlideViewDemo = () => {
   let proxyUrl = 'https://cors-anywhere.herokuapp.com/';
   let targetUrl = "https://yts.am/api/v2/list_movies.json?sort_by=download_count";
   const list = useFetch(proxyUrl,targetUrl)
   if(list.loading) return <div>Loading...</div>
-  return <ListView list={list.datas} type="slide" width={800} height={150} widthNum={5}/>
+  return <ListView list={list.datas} type="slide" widthNum={5} width={1000} height={170}/>
 }
 storiesOf('ListView|SlideView', module)
+.add('Docs', () => <ShowDocs md={require('../docs/a.md')} />)
 .add('default', () => <DefaultSlideViewDemo/>
 );
