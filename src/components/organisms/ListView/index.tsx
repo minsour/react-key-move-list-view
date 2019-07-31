229cfe113d3564f3c502f9809c85d74244b8141d
devsoopark <sparkm0128@gmail.com>
2019-07-31 10:34:41 +0900

    FIX: fix matrix height error

--- src/components/organisms/ListView/index.tsx
+++ src/components/organisms/ListView/index.tsx
@@ -22,7 +22,7 @@ const ListView = (props: IListViewProps) => {
   const list = useList(props.list);
   const windowDimension = useWindowDimensions();
   const width = (props.width===void 0) ? windowDimension.width : props.width;
-  const height = (props.height===void 0) ? (windowDimension.width/5.4-10)/6*5 : props.height;
+  const height = (props.height===void 0) ? (windowDimension.width/5.4)/6*5*2 : props.height;
 
   return (
     props.type === VIEW_TYPE.SLIDE ?
